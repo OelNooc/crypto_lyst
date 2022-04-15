4060c2a917b6d94f76a753d156542e2031157de4
Leonardo Catejo <leo.catejoa@gmail.com>
2022-04-14 20:52:23 -0400

    Se insertan viewbindings, y handler para splashFragment

--- app/src/main/java/cl/nooc/cryptolyst/ui/DetailFragment.kt
+++ app/src/main/java/cl/nooc/cryptolyst/ui/DetailFragment.kt
@@ -6,15 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cl.nooc.cryptolyst.R
+import cl.nooc.cryptolyst.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/cl/nooc/cryptolyst/ui/WelcomeFragment.kt
+++ app/src/main/java/cl/nooc/cryptolyst/ui/WelcomeFragment.kt
@@ -1,20 +1,32 @@
 package cl.nooc.cryptolyst.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import cl.nooc.cryptolyst.R
+import cl.nooc.cryptolyst.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            Navigation.findNavController(requireView())
+                .navigate(R.id.action_welcomeFragment_to_homeFragment)
+        }, 5000)
+        return binding.root
     }
 
 }
